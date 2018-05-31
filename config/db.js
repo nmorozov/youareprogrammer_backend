14da9801b810eff118616ddbc2a3e442a82001f7
nmorozov <nikmorze@gmail.com>
2018-05-31 22:59:29 +0400

    mongodb connection and config

---
+++ config/db.js
@@ -0,0 +1,3 @@
+const CONNECTION_URL = 'mongodb://mongo:qwerty1@ds016148.mlab.com:16148/youareprogrammer';
+
+export default CONNECTION_URL;
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+import bodyParser from 'body-parser';
 
 import routes from './app/routes';
+import CONNECTION_URL from './config/db';
 
 const app = express();
 const port = 8000;
 
-routes(app, {});
+const dbName = 'youareprogrammer';
 
-app.listen(port, () => {
-  console.log(`Server started on ${port}`);
+app.use(bodyParser.json());
+
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err);
+
+  const db = client.db(dbName);
+
+  routes(app, db);
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+  });
+
+  return true;
 });
